cc7df36c4cd349cc06607e0f3619312ecc8219cc
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-09-03 14:19:43 +0700

    Enhance validation middleware to check for missing category and content, and fix typo in error message for invalid category.

--- server/middleware/validation.mjs
+++ server/middleware/validation.mjs
@@ -5,13 +5,15 @@ const validation = (req, res, next) => {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (!emailRegex.test(email))
     return res.status(400).json({ message: "Invalid Email" });
+  if (!category) return res.status(400).json({ message: "Missing Category" });
   if (category !== "Math" && category !== "English" && category !== "Biology")
-    return res.status(400).json({ message: "Invalid Categoty" });
-  if (content.length < 500 && content.length > 1000)
+    return res.status(400).json({ message: "Invalid Category" });
+  if (!content) return res.status(400).json({ message: "Missing Centent" });
+  if (content.length < 500 || content.length > 1000)
     return res
       .status(400)
       .json({ message: "Content must be between 500-1000 letters" });
   next();
 };
 
-export default validation
+export default validation;
